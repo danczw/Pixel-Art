0538ac985a9df7dbafbee6f774f33c1b2ab01e45
DCZ <daniel.czwalinna@alexanderthamm.com>
2021-03-26 11:23:28 +0100

    added Vue app

---
+++ pixel_art_app/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <p>hi</p>
+    </div>
+
+    <div class="nav">
+      <div class="nav__item">
+        <p><router-link to="/">Home</router-link></p>
+      </div>
+      <div class="nav__item">
+        <p><router-link to="/about">About</router-link></p>
+      </div>
+    </div>
+    <router-view />
+  </div>
+  
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  
+    .nav {
+      padding: 10px;
+      width: 50px;
+      height: 90vh;
+      position: absolute;
+      right: 0;
+
+      .nav__item {
+        // margin: 10px;
+        min-height: 60px;
+
+        p {
+          transform: rotate(90deg);
+          text-anchor: middle;
+        }
+      }
+    }
+    .nav a {
+      font-weight: bold;
+      color: #2c3e50;
+    }
+
+    .nav a.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ pixel_art_app/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <bwArt/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import bwArt from "@/components/bwArt.vue";
+
+export default {
+  name: "Home",
+  components: {
+    bwArt,
+  },
+};
+</script>
