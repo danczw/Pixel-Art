1f067a908d5a21ef1db58d098034d330f372a002
danczw <devdancz@pm.me>
2021-04-03 03:58:03 +0200

    added background effect

--- src/components/particle.vue
+++ src/components/particle.vue
@@ -3,6 +3,7 @@
     <div class="art">
       <canvas id="canvas1"></canvas>
       <canvas id="canvas2"></canvas>
+      <canvas id="canvas3"></canvas>
     </div>
   </div>
 </template>
@@ -12,6 +13,7 @@ export default {
   name: "particle",
   setup() {},
   mounted() {
+    this.background();
     this.effect();
     this.effect_two();
   },
@@ -278,6 +280,98 @@ export default {
         }
       }
     },
+    background() {
+      const canvas = document.getElementById("canvas3");
+      const ctx = canvas.getContext("2d");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      let particleArray = [];
+
+      // create particle
+      class Particle {
+        constructor(x, y, directionX, directionY, size, color) {
+          this.x = x;
+          this.y = y;
+          this.directionX = directionX;
+          this.directionY = directionY;
+          this.size = size;
+          this.color = color;
+        }
+        // draw individual particle
+        draw() {
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+          ctx.fillStyle = "rgba(25, 255, 255, 0.2)";
+          ctx.fill();
+        }
+        update() {
+          if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+          }
+          if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+          }
+          // move particles
+          this.x += this.directionX;
+          this.y += this.directionY;
+          // draw particles
+          this.draw();
+        }
+      }
+      function init() {
+        particleArray = [];
+        let numberOfParticles = (canvas.height * canvas.width) / 15000;
+        for (let i = 0; i < numberOfParticles; i++) {
+          let size = Math.random() * 5 + 1;
+          let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
+          let y =
+            Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
+          let directionX = Math.random() * 5 - 2.5;
+          let directionY = Math.random() * 5 - 2.5;
+          let color = "rgba(25, 255, 255, 0.2)";
+
+          particleArray.push(
+            new Particle(x, y, directionX, directionY, size, color)
+          );
+        }
+      }
+      init();
+      animate();
+      // check if particles are close enough and connect
+      function connect() {
+        let opacityValue = 1
+        for (let a = 0; a < particleArray.length; a++) {
+          for (let b = 0; b < particleArray.length; b++) {
+            let distance = (
+              ((particleArray[a].x - particleArray[b].x)
+              * (particleArray[a].x - particleArray[b].x))
+              + ((particleArray[a].y - particleArray[b].y)
+              * (particleArray[a].y - particleArray[b].y))
+            )
+            if (distance < canvas.width/10 * canvas.height/10) {
+              opacityValue = 1 - (distance / 10000);
+              ctx.strokeStyle = `rgba(25, 75, 75, ${opacityValue})`
+              ctx.lineWidth = 1;
+              ctx.beginPath;
+              ctx.moveTo(particleArray[a].x, particleArray[a].y);
+              ctx.lineTo(particleArray[b].x, particleArray[b].y)
+              ctx.stroke();
+            }
+          }
+        }
+      }
+
+      function animate() {
+        requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+        for (let i = 0; i < particleArray.length; i++) {
+          particleArray[i].update();
+        }    
+        connect();
+      }
+    },
   },
 };
 </script>
@@ -291,6 +385,12 @@ export default {
 }
 
 #canvas2 {
+  top: 0;
+  left: 0;
+  position: absolute;
+}
+
+#canvas3 {
   top: 0;
   left: 0;
   position: absolute;
