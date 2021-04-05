472ce953e3bfc393f1e1745d979fa1db5676267c
danczw <devdancz@pm.me>
2021-04-06 01:02:58 +0200

    added tip links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="wrapper__home">
+    <div id="tbd">
+      <div>
+        <p>
+          Tip Nano:
+          nano_3jpa68851np7yzfwdzentxzr1rai9eftni4sabxogrruttjws7y1mhhrqu7c
+        </p>
+        <p>Tip BTC: coming soon</p>
+      </div>
+    </div>
+    <div class="loader__wrapper">
+      <atom-spinner :animation-duration="1000" :size="60" :color="'#ff1d5e'" />
+    </div>
   </div>
 </template>
+
+<script>
+import { AtomSpinner } from "epic-spinners";
+
+export default {
+  name: "about",
+  components: {
+    AtomSpinner,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper__home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: white;
+  font-size: 20px;
+  width: 100vw;
+  text-align: left;
+  font-family: "Lato";
+
+  #tbd {
+    padding-left: 20%;
+  }
+
+  .loader__wrapper {
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 50px;
+  }
+}
+</style>
