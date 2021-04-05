fd8d33775ab0a816ad8cb1d605e0fcccec7c5ccc
danczw <devdancz@pm.me>
2021-04-06 01:11:55 +0200

    fixed routing typo

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,13 @@ const routes = [
     path: "/",
     name: "Home",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/home.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Home.vue"),
   },
   {
     path: "/about",
     name: "About",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/about.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
     path: "/particle",
